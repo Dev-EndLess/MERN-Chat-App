81473265284b92c4aabe857c67184eb6bf80a9ac
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-08-21 19:23:29 +0200

    mongodb/server setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 
-const app = express();
-dotenv.config();
+import authRouters from "./routes/auth.routes.js";
+import mongoDBConnection from "./db/mongoDBConnection.js";
 
+const app = express();
 const PORT = process.env.PORT || 5000
 
+dotenv.config();
+
+app.use(express.json()) // to parse the incoming requests with JSON payloads
+
+app.use("/api/auth", authRouters)
+
+app.use((req, res, next) => {
+  console.log("Request Method:", req.method);
+  console.log("Request Headers:", req.headers);
+  console.log("Request Body:", req.body);
+  next();
+});
+
+
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  mongoDBConnection()
+  console.log(`Server started on port ${PORT}`)
 })
+
