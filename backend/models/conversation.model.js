a61d2e979be2685de01cf7cf1ca0f6c3b56ca636
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-08-26 08:08:29 +0200

    added conversation.model

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose
+
+const conversationSchema = new Schema(
+	{
+		participants: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "User",
+				required: true,
+			},
+		],
+		messages: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Message",
+				default: [],
+			},
+		],
+	},
+	{ timestamps: true }
+);
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+export default Conversation;
