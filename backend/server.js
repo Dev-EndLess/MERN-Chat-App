85530555004fd8f6d93f8952bf7139b523817fdf
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-08-26 08:14:03 +0200

    messages / users routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoDBConnection from "./db/mongoDBConnection.js";
 
 import authRouters from "./routes/auth.routes.js";
-import mongoDBConnection from "./db/mongoDBConnection.js";
+import messageRouters from "./routes/message.routes.js";
+import userRouters from "./routes/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -10,8 +13,11 @@ const PORT = process.env.PORT || 5000
 dotenv.config();
 
 app.use(express.json()) // to parse the incoming requests with JSON payloads
+app.use(cookieParser())
 
 app.use("/api/auth", authRouters)
+app.use("/api/messages", messageRouters)
+app.use("/api/users", userRouters)
 
 app.use((req, res, next) => {
   console.log("Request Method:", req.method);
