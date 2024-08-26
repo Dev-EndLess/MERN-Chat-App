7954c32a6aac843cfd0adcd93661a6c3e5d075cb
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-08-26 08:10:33 +0200

    added user.controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js";
+
+export const getUsersSideBar = async (req, res) => {
+  try {
+    const loggedInUser = req.user._id
+
+    /* ne = not equal - see https://www.mongodb.com/docs/manual/reference/operator/query/ */
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUser } }).select("-password")
+
+    res.status(200).json(filteredUsers);
+
+  } catch (error) {
+    console.error({ "Error in getUsersSideBar: ": error.message })
+    res.status(500).json({ error: "Internal Server Error" })
+  }
+}
