7b69595d7455b11980e43007f01a8d133eb2e684
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2024-08-26 08:08:13 +0200

    added message.model

---
+++ backend/models/message.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose
+
+const messageSchema = new Schema(
+  {
+    senderId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message
