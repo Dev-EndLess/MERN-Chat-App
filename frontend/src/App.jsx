c881671fb357d4100d994a743667f3caa5e15197
Fabrizio Pozzi <endlesspain1988@gmail.com>
2024-08-29 16:51:45 +0200

    added home/signup/login routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import SignUp from "./pages/signup/SignUp"
+import Login from "./pages/login/Login"
+import Home from "./pages/home/Home"
+import { Toaster } from "react-hot-toast"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { useAuthContext } from "./context/AuthContext"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { authUser } = useAuthContext()
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full h-screen p-4">
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          exact
+          path="/signup"
+          element={authUser ? <Navigate to="/login" /> : <SignUp />}
+        />
+        <Route
+          exact
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+      </Routes>
+      <Toaster position="bottom-center" />
+    </div>
   )
 }
 
