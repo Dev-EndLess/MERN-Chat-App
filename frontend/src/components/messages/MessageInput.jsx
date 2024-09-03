d7d358df1243ad54555f09b8aef67eb42242236d
Fabrizio Pozzi <endlesspain1988@gmail.com>
2024-09-03 07:38:37 +0200

    POST send and render messages

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,26 +1,41 @@
-// import { useState } from "react";
-import { BsSend } from "react-icons/bs";
+import { useState } from "react"
+import { BsSend } from "react-icons/bs"
+import useSendMessage from "../../hooks/useSendMessage"
 
 const MessageInput = () => {
-  // const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("")
+  const { loading, sendMessage } = useSendMessage()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    if (!message) return
+    await sendMessage(message)
+    setMessage("")
+  }
 
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="relative w-full">
         <input
           type="text"
-          className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
+          className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-emerald-200 focus:border-emerald-200 focus:outline-none"
           placeholder="Send a message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
         />
         <button
           type="submit"
           className="absolute top-3 right-4 text-lg text-emerald-200"
         >
-          <BsSend />
+          {loading ? (
+            <div className="loading loading-spinner"></div>
+          ) : (
+            <BsSend />
+          )}
         </button>
       </div>
     </form>
-  );
-};
+  )
+}
 
-export default MessageInput;
+export default MessageInput
---
+++ frontend/src/hooks/useSendMessage.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { useConversationsContext } from "../context/GlobalContext"
+import toast from "react-hot-toast"
+
+const useSendMessage = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const { messages, setMessages, selectedConversation } =
+    useConversationsContext()
+
+  const sendMessage = async (message) => {
+    setIsLoading(true)
+
+    try {
+      const res = await fetch(`/api/messages/send/${selectedConversation._id}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ message }),
+        }
+      )
+
+      const data = await res.json()
+      if (data.error) throw new Error(data.error)
+
+      setMessages([...messages, data])
+
+    } catch (error) {
+      toast.error(error.message)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  return { sendMessage, isLoading }
+}
+
+export default useSendMessage
