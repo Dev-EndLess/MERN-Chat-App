8575180ef85f073f4cb7a9142b4ce59834a66797
Fabrizio Pozzi <endlesspain1988@gmail.com>
2024-09-04 18:45:34 +0200

    added socket.IO

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,8 @@
 import Conversation from "../models/conversation.model.js"
 import Message from "../models/message.model.js"
+import { getReceiverSocketId, io } from "../socket/socket.js"
 
 export const sendMessage = async (req, res) => {
-
   try {
     const { message } = req.body
     const { id: receiverId } = req.params
@@ -23,7 +23,7 @@ export const sendMessage = async (req, res) => {
     const newMessage = new Message({
       senderId,
       receiverId,
-      text: message
+      text: message,
     })
 
     if (newMessage) {
@@ -32,8 +32,14 @@ export const sendMessage = async (req, res) => {
 
     await Promise.all([conversation.save(), newMessage.save()])
 
-    res.status(201).json(newMessage)
+    /* * Socket.IO * */
+    const receiverSocketId = getReceiverSocketId(receiverId)
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage)
+    }
 
+    res.status(201).json(newMessage)
   } catch (error) {
     console.log("Error in sendMessage Controller: ", error.message)
     res.status(500).json({ error: "Internal Server Error" })
@@ -41,21 +47,21 @@ export const sendMessage = async (req, res) => {
 }
 
 export const getMessages = async (req, res) => {
-	try {
-		const { id: userToChatId } = req.params
-		const senderId = req.user._id
+  try {
+    const { id: userToChatId } = req.params
+    const senderId = req.user._id
 
-		const conversation = await Conversation.findOne({
-			participants: { $all: [senderId, userToChatId] },
-		}).populate("messages") // NOT REFERENCE BUT ACTUAL MESSAGES
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages") // NOT REFERENCE BUT ACTUAL MESSAGES
 
-		if (!conversation) return res.status(200).json([])
+    if (!conversation) return res.status(200).json([])
 
-		const messages = conversation.messages
+    const messages = conversation.messages
 
-		res.status(200).json(messages)
-	} catch (error) {
-		console.log("Error in getMessages controller: ", error.message)
-		res.status(500).json({ error: "Internal server error" })
-	}
+    res.status(200).json(messages)
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message)
+    res.status(500).json({ error: "Internal server error" })
+  }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import mongoDBConnection from "./db/mongoDBConnection.js";
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import mongoDBConnection from "./db/mongoDBConnection.js"
 
-import authRouters from "./routes/auth.routes.js";
-import messageRouters from "./routes/message.routes.js";
-import userRouters from "./routes/user.routes.js";
+import authRouters from "./routes/auth.routes.js"
+import messageRouters from "./routes/message.routes.js"
+import userRouters from "./routes/user.routes.js"
+import { app, server } from "./socket/socket.js"
 
-const app = express();
+// const app = express()
 const PORT = process.env.PORT || 5000
 
-dotenv.config();
+dotenv.config()
 
 app.use(express.json()) // to parse the incoming requests with JSON payloads
 app.use(cookieParser())
@@ -20,15 +21,13 @@ app.use("/api/messages", messageRouters)
 app.use("/api/users", userRouters)
 
 app.use((req, res, next) => {
-  console.log("Request Method:", req.method);
-  console.log("Request Headers:", req.headers);
-  console.log("Request Body:", req.body);
-  next();
-});
+  console.log("Request Method:", req.method)
+  console.log("Request Headers:", req.headers)
+  console.log("Request Body:", req.body)
+  next()
+})
 
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   mongoDBConnection()
   console.log(`Server started on port ${PORT}`)
 })
-
