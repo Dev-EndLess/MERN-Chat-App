8818aba536c5488b08b361bea92ed66801864531
Fabrizio Pozzi <endlesspain1988@gmail.com>
2024-09-10 17:52:07 +0200

    sort contact list

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,12 +1,27 @@
 import Conversation from "./Conversation"
+import useGetConversations from "../../hooks/useGetConversations"
+import { useSocketContext } from "../../context/SocketContext"
 import { useConversationsContext } from "../../context/GlobalContext"
 
 function Conversations() {
   const { isLoading, filteredConversations } = useConversationsContext()
+  const { onlineUsers } = useSocketContext()
+
+  const sortedConversations = [...filteredConversations].sort((a, b) => {
+    const isAOnline = onlineUsers.includes(a._id.toString())
+    const isBOnline = onlineUsers.includes(b._id.toString())
+
+    // Se A è online e B no, A viene prima
+    if (isAOnline && !isBOnline) return -1
+    // Se B è online e A no, B viene prima
+    if (!isAOnline && isBOnline) return 1
+    // Se entrambi sono online o offline, l'ordine non cambia
+    return 0
+  })
 
   return (
     <div className="flex md:h-[380px] flex-col py-2 overflow-y-auto">
-      {filteredConversations.map((conversation, index) => (
+      {sortedConversations.map((conversation, index) => (
         <Conversation key={index} conversation={conversation} />
       ))}
       {isLoading ? (
